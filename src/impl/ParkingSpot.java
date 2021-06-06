3903bddee449d8d7fdb06f31d1a28718cb6737ff
satyam-sm <satyam101905@gmail.com>
2021-06-05 23:59:25 -0700

    adding question

--- src/impl/ParkingSpot.java
+++ src/impl/ParkingSpot.java
@@ -1,4 +1,11 @@
 package impl;
 
+import abstractClass.Vehicle;
+import enums.VehicleSizeEnum;
+
 public class ParkingSpot {
+    private Vehicle vehicle;
+    private VehicleSizeEnum spotSize;
+    private int row;
+    private int spotNumber;
 }
