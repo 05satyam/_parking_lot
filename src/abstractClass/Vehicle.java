6903b1ec55f83fe82aefa8139abdbded6451bf02
satyam-sm <satyam101905@gmail.com>
2021-06-05 23:55:53 -0700

    basic structure

---
+++ src/abstractClass/Vehicle.java
@@ -0,0 +1,10 @@
+package abstractClass;
+
+import enums.VehicleSizeEnum;
+
+
+public class Vehicle {
+    protected String licensePlate;
+    protected int spotsNeeded;
+    protected VehicleSizeEnum size;
+}
